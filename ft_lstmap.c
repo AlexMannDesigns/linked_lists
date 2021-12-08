9b827cc1b0b2e7f4a03bd2255c3a9ea7a66e8baa
AlexMannDesigns <alex.mann1989@gmail.com>
2021-12-08 13:47:46 +0200

    removed comment from lstmap, testing git credentials saved in WSL

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,9 +3,7 @@
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	//iterate list and apply 'f' to each link, creating a 'fresh' list from the result
-	//remember malloc protection
-	t_list	*temp;
+	t_list *temp;
 	temp = f(ft_lstnew(lst->content, lst->content_size));
 	return temp;
 }
-
